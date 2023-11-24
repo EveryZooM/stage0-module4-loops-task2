d98841fc4e812a95213bf4b7197787a1ef08f43f
ZooM <yuhos@mail.ru>
2023-11-24 16:14:16 +0300

    How many workarounds have I used here? I've already lost count

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        int counter = 0;
+        while (counter <= printToInclusive) {
+            if (counter == 2 || counter == 3 || counter == 5) {
+                System.out.println(counter);
+                counter++;
+            } else if (counter == 1 || counter % 2 == 0 || counter % 3 == 0 || counter % 5 == 0) {
+                counter++;
+            } else {
+                System.out.println(counter);
+                counter++;
+            }
+        }
     }
 }
